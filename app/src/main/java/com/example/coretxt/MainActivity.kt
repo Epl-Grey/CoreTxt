e0ab024d0eb083e81baa6dcbbfa75ebece8ef8d2
a4ifka <ilyha.borec@gmail.com>
2023-05-07 15:20:13 +0600

    test

--- app/src/main/java/com/example/coretxt/MainActivity.kt
+++ app/src/main/java/com/example/coretxt/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
-
+// test
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
