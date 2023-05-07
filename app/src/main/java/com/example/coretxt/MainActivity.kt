505ce8dd6dfc35549f72a5ae74519191de2a1068
Ilia <ilyha.borec@mgail.com>
2023-05-07 08:41:19 +0600

    fix SplashScreen and add menu(test)

--- app/src/main/java/com/example/coretxt/MainActivity.kt
+++ app/src/main/java/com/example/coretxt/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.example.coretxt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var btnAi: Button = findViewById(R.id.btnAi)
+        val intent = Intent(this, MainActivity::class.java)
+         btnAi.setOnClickListener {
+             startActivity(intent)
+         }
+
+        var btnExit: Button = findViewById(R.id.exit)
+
+        btnExit.setOnClickListener {
+            System.exit(-1)
+        }
 
     }
 }
--- app/src/main/java/com/example/coretxt/SplashScreenActivity.kt
+++ app/src/main/java/com/example/coretxt/SplashScreenActivity.kt
@@ -3,6 +3,9 @@ package com.example.coretxt
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 import android.widget.TextView
 
 //import kotlinx.android.synthetic.main.activity.splash_screen.*
@@ -12,13 +15,22 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        var coreTxt = TextView(findViewById(R.id.coreTxt))
-        coreTxt.alpha = 0f
-        coreTxt.animate().setDuration(1500).alpha(1f).withEndAction{
-            val i = Intent(this, MainActivity::class.java)
-            startActivity(i)
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        //Normal Handler is deprecated , so we have to change the code little bit
+
+        // Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
             finish()
-        }
+        }, 3000) // 3000 is the delayed time in milliseconds.
     }
 }
